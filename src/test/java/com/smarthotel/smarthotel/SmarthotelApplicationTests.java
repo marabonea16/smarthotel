99f7f12e82c5ac996eb35322cc7d0e51da902aba
marabonea16 <163434333+marabonea16@users.noreply.github.com>
2024-05-06 19:08:22 +0300

    initial commmit

---
+++ src/test/java/com/smarthotel/smarthotel/SmarthotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthotel.smarthotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
