db354c28ba690e801318e3d43e66431932557b60
Mara <marabonea16@gmail.com>
2024-05-26 17:10:30 +0300

    login try

---
+++ src/main/resources/static/public/js/app.js
@@ -0,0 +1,22 @@
+// Function to handle sign-in
+document.getElementById('signInBtn').addEventListener('click', signIn);
+
+function signIn() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log('User signed in:', user);
+            // Redirect to dashboard or perform other actions
+             window.location.href = 'home.html';
+        })
+        .catch((error) => {
+            // Handle sign-in errors
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error('Sign-in error:', errorMessage);
+        });
+}
